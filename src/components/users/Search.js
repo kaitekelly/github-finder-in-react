d5eae31d650d53fc5f2f225f55de813c04bdaf52
kaitekelly <murphy.kaite@gmail.com>
2021-05-13 14:18:30 -0700

    clear search button and function

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,7 +8,9 @@ export class Search extends Component {
   };
 
   static propTypes = {
-    searchUsers: PropTypes.func.isRequired
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
 
   onSubmit = (e) => {
@@ -20,6 +22,7 @@ export class Search extends Component {
   onChange = (e) => this.setState({ text: e.target.value });
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className='form'>
@@ -36,6 +39,10 @@ export class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {this.props.showClear && <button className="btn btn-light btn-block" onClick={this.props.clearUsers} >
+          Clear
+        </button>}
+        
       </div>
     );
   }
